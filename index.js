63a53cbea6182b9bc6bd6425eb9078824e3f6fdd
Mohak Khandelwal <113772362+Maxginamoto@users.noreply.github.com>
2025-09-24 14:01:51 +0530

    updating dotenv

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 const cors = require('cors');
-
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
