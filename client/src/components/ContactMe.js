eb18ac9e182a7a10bed620fd61e8056df72f5314
Mohak Khandelwal <113772362+Maxginamoto@users.noreply.github.com>
2025-10-01 20:22:55 +0530

    Update ContactMe.js

--- client/src/components/ContactMe.js
+++ client/src/components/ContactMe.js
@@ -3,7 +3,7 @@ function ContactMe() {
     return (<>
         <h1>Contact Us Page</h1>
         <p>
-            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=someone@example.com&su=Feedback+regarding+Gridlock+Games&tf=cm">Contact via email</a>
+            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=mohak.khandu@gmail.com&su=Feedback+regarding+Gridlock+Games&tf=cm">Contact via email</a>
         </p>
     </>);
 }
